3e66675b785686469f64af00fd97451b8e24e116
Kadiev99 <kadiev_1999@abv.bg>
2023-01-26 17:23:54 +0200

    Initial commit

---
+++ src/EmailApp.java
@@ -0,0 +1,9 @@
+public class EmailApp {
+    public static void main(String[] args) {
+        // You are an IT Support Administrator Specialist and are charged with task of creating email accounts for new hired.
+
+        Email email = new Email("John", "Smith");
+
+        System.out.println(email.showInfo());
+    }
+}
